3b88ff0cfe44cc0a1b71a8b03732e4b5c18a363f
idavidcastro <89281605+idavidcastro@users.noreply.github.com>
2024-03-31 14:11:21 -0500

    Agregar archivos de proyecto.

---
+++ ProveedoresAPI/Models/Proveedor.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProveedoresAPI.Models
+{
+    public class Proveedor
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string NIT { get; set; }
+        public string RazonSocial { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public string Departamento { get; set; }
+        public string Correo { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string NombreContacto { get; set; }
+        public string CorreoContacto { get; set; }
+    }
+}
---
+++ ProveedoresAPI/Repositories/ProveedorRepository.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+
+namespace ProveedoresAPI.Repositories
+{
+    public class ProveedorRepository
+    {
+        public MongoClient client;
+        public IMongoDatabase db;
+        public ProveedorRepository()
+        {
+            client = new MongoClient("");
+        }
+    }
+}
